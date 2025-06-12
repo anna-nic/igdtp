f135c5321a00914ff459de2515f17968b741da9f
Zebbedii <114523557+Zebbedii@users.noreply.github.com>
2025-06-12 19:35:37 +0100

    Update launch_openmc_jupyter.sh
    added description on how to remove token requirements from jupyter

--- launch_openmc_jupyter.sh
+++ launch_openmc_jupyter.sh
@@ -24,7 +24,24 @@ fi
 wait
 
 
+###############
+###############
+###############
 
+#Option to remove token from jupyter 
 
+#File path -  cd /home/user/miniforge3/envs/OpenMC-Env01/lib/python3.11/site-packages/notebook_shim/tests/confs/
+
+#edit - jupyter_notebook_config.py  
+## Don't require token for browser
+#c.NotebookApp.token = ''
+#c.NotebookApp.password = ''
+
+## Automatically open in browser
+#c.NotebookApp.open_browser = True
+
+## Set the IP and port
+#c.NotebookApp.ip = 'localhost'
+#c.NotebookApp.port = 8888  # or any available port
 
 
